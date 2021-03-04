8fcfcf4f21b0c393226b0e05710ac5f7e06facc8
f.tanaka <nitrow11@yahoo.co.jp>
2021-03-04 21:19:40 +0900

    不要なコメント削除

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,18 +1,14 @@
 import consumer from "./consumer"
 
 
-// 「const appRoom =」を追記
 const appRoom = consumer.subscriptions.create("RoomChannel", {
-  // 省略
 
   received(data) {
     const messages = document.getElementById('messages');
-    
     messages.insertAdjacentHTML('beforeend', data['message']);
   },
 
   speak: function(message) {
-    //alert("speak");
     return this.perform('speak', {message: message});
   }
 });
